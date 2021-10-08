ecc3b7203a6b16f82e045c09e3bc5e9cca9ba427
manjea <manjea@edu.umea.se>
2021-10-08 10:17:30 +0200

    mer php kod :))))

--- phpNyttKap/KAPITEL5555555555555555!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!/nån demo/functions.php
+++ phpNyttKap/KAPITEL5555555555555555!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!/nån demo/functions.php
@@ -3,8 +3,16 @@
         $data = trim($x);
         $data = stripslashes($data);
         $data = htmlspecialchars($data);
+
+        return $data;
     }
 
+    function numberChecker($x){
+        if(is_numeric($x) && $x > 0){
+            return true;
+        }
+        return false;
+    }
 
 
 ?>
--- phpNyttKap/KAPITEL5555555555555555!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!/nån demo/test.php
+++ phpNyttKap/KAPITEL5555555555555555!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!/nån demo/test.php
@@ -1,8 +1,17 @@
 <?php
-include(functions.php);
+    require('functions.php');
 
     if (isset($_POST['t'])){
+        //sanitizar input :)
         $input = sanitize($_POST['t']);
+
+        //om tal > 0
+        if(numberChecker($input)){
+            echo $input . " är ett tal";
+        }else{
+            echo $input;
+        }
+        echo "<br>";
     }else{
 
     }
