b668c7e5955222a02d202c536913be6758f9eaf1
manjea <manjea@edu.umea.se>
2021-10-19 14:39:36 +0200

    fixar lite tack såmycket milton

--- phpNyttKap/KAPITELLLLL666666666KAKORRRR!!!!!11!!1!!1!/övn5/cookies.php
+++ phpNyttKap/KAPITELLLLL666666666KAKORRRR!!!!!11!!1!!1!/övn5/cookies.php
@@ -1,11 +1,11 @@
 <?php
     if(isset($_POST['sub'])){
-        if(!isset($_COOKIE['firstName'])){
+        if(isset($_COOKIE['firstName']) && isset($_COOKIE['lastName'])){
+            echo 'Du har redan fyllt i detta formulär ' . $_COOKIE['firstName'] . ' ' . $_COOKIE['lastName'] . '<br>';
+        }else{
             setcookie('firstName', $_POST['forn'], time() + 60*60, "/");
             setcookie('lastName', $_POST['eftn'], time() + 60*60, "/");
             echo 'tack för din registrering ' . $_POST['forn'] . '<br>';
-        }else{
-            echo 'Du har redan fyllt i detta formulär ' . $_COOKIE['firstName'] . ' ' . $_COOKIE['lastName'] . '<br>';
         }
         
     }
