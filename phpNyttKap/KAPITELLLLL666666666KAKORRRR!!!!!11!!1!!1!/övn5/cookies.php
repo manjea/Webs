06c1df099215e59a85860f2b2bbcdb34fb8b3e72
manjea <manjea@edu.umea.se>
2021-10-19 14:41:42 +0200

    beutifulasdlkjf

--- phpNyttKap/KAPITELLLLL666666666KAKORRRR!!!!!11!!1!!1!/övn5/cookies.php
+++ phpNyttKap/KAPITELLLLL666666666KAKORRRR!!!!!11!!1!!1!/övn5/cookies.php
@@ -1,13 +1,14 @@
 <?php
     if(isset($_POST['sub'])){
         if(isset($_COOKIE['firstName']) && isset($_COOKIE['lastName'])){
-            echo 'Du har redan fyllt i detta formulär ' . $_COOKIE['firstName'] . ' ' . $_COOKIE['lastName'] . '<br>';
-        }else{
-            setcookie('firstName', $_POST['forn'], time() + 60*60, "/");
-            setcookie('lastName', $_POST['eftn'], time() + 60*60, "/");
-            echo 'tack för din registrering ' . $_POST['forn'] . '<br>';
+            if($_COOKIE['firstName']) == $_POST['forn'] && $_COOKIE['lastName']) == $_POST['eftn']){
+                echo 'Du har redan fyllt i detta formulär ' . $_COOKIE['firstName'] . ' ' . $_COOKIE['lastName'] . '<br>';
+            }else{
+                setcookie('firstName', $_POST['forn'], time() + 60*60, "/");
+                setcookie('lastName', $_POST['eftn'], time() + 60*60, "/");
+                echo 'tack för din registrering ' . $_POST['forn'] . '<br>';
+            }
         }
-        
     }
     echo '<a href="form.php">tillbaka</a>'; // lol
     
